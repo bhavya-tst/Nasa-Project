ca4c42c98f62a30919517d2b9ace726c03e24f3a
bhavya-tst <bhavya.patel@tsttechnology.io>
2025-02-01 18:26:00 +0530

    inital commit

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,29 @@
+const {parse}=require('csv-parse');
+const fs=require('fs');
+
+const habitablePlanets=[];
+function isHabitablePlanet(planet){
+    return planet['koi_disposition']==="CONFIRMED" 
+    && planet['koi_insol'] >0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6  
+
+}
+fs.createReadStream("kepler_data.csv")
+.pipe(parse({
+    comment:"#",
+    columns:true,
+}))
+.on("data",(data)=>{
+    if(isHabitablePlanet(data))
+        habitablePlanets.push(data);
+})
+.on("end",()=>{
+console.log("There are only "+habitablePlanets.length+" habitable as earth.");
+})
+.on("error",(err)=>{
+    console.log("Error: ",err);
+})
+
+module.exports={
+    planets:habitablePlanets
+}
